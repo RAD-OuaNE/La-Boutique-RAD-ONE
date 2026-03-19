ee842af92ca7957ce888cf16646c1e16b86e59cd
rachidben91-web <rachid.ben91@gmail.com>
2026-03-19 16:38:23 +0100

    Update app-config.js

--- js/app-config.js
+++ js/app-config.js
@@ -1,6 +1,6 @@
 window.REDONE_CONFIG = window.REDONE_CONFIG || {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://nbakekcxtyvfnejvgfli.supabase.co",
+  supabaseAnonKey: "sb_publishable_s6j4qJiyjdwku6JDznobKg_GtdKuImP",
   productsTable: "products",
   ordersTable: "orders",
   storageBucket: "product-images",
